0bcf3ff6b78ad077a2677fc677a403d30c7cc378
f16011938 <robinfournier0998@gmail.com>
2018-11-26 17:34:22 +0100

    Tache 1 à revoir (fini)

---
+++ src/main/java/viewer/BruteRasterImage.java
@@ -0,0 +1,92 @@
+package viewer;
+
+import image.Image;
+import javafx.scene.paint.Color;
+import util.Matrices;
+
+public class BruteRasterImage implements Image {
+
+    Color[][] colors;
+    Color color;
+    int width;
+    int height;
+
+    public BruteRasterImage(Color color, int width, int height) {
+        this.width = width;
+        this.height = height;
+        colors = new Color[width][height];
+        for(int x = 0 ; x < this.width; x++) {
+            for(int y = 0; y < this.height; y++) {
+                colors[x][y] = color;
+            }
+        }
+    }
+
+    public BruteRasterImage(Color[][] colors){
+        width = Matrices.getColumnCount(colors);
+        height = Matrices.getRowCount(colors);
+        this.colors = new Color[width][height];
+        for(int x = 0 ; x < this.width; x++) {
+            for(int y = 0; y < this.height; y++) {
+                this.colors[x][y] = colors[x][y];
+            }
+        }
+    }
+
+
+    public void createRepresentation(){
+        this.colors = new Color[this.width][this.height];
+    }
+
+
+    public void setPixelColor(Color color, int x, int y){
+        this.color = colors[x][y];
+    }
+
+    public void setPixelsColor(Color[][] pixels) {
+        width = Matrices.getRowCount(colors);
+        height = Matrices.getColumnCount(colors);
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                this.colors[x][y] = pixels[x][y];
+            }
+        }
+    }
+
+    private void setPixelsColor(Color color){
+        width = Matrices.getColumnCount(colors);   //largeur
+        height = Matrices.getRowCount(colors);     //hauteur
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                this.colors[x][y] = color;
+            }
+        }
+    }
+
+    @Override
+    public Color getPixelColor(int x, int y) {
+        return colors[x][y];
+    }
+
+    @Override
+    public int getWidth() {
+        return width;
+    }
+
+    @Override
+    public int getHeight() {
+        return height;
+    }
+
+    protected int setWidth(int width){
+        this.width = width;
+        return 0;
+    }
+
+    protected int setHeight(int height){
+        this.height = height;
+        return 0;
+    }
+
+
+}
